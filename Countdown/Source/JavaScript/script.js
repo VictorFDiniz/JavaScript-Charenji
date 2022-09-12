cc9bc063c3c57eee108e60860047c3e0a6c53c87
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2022-09-12 13:36:00 -0300

    initial commit

--- Countdown/Source/JavaScript/script.js
+++ Countdown/Source/JavaScript/script.js
@@ -1,21 +1,21 @@
-const daysEle = document.getElementById('days')
-const hoursEle = document.getElementById('hours')
-const minutesEle = document.getElementById('minutes')
-const secondsEle = document.getElementById('seconds')
+const daysEle = document.getElementById('days');
+const hoursEle = document.getElementById('hours');
+const minutesEle = document.getElementById('minutes');
+const secondsEle = document.getElementById('seconds');
 
-const halloween = "31 Oct 2022"
+const halloween = "31 Oct 2022";
 
 function countdown() {
 	
 	const halloweenDate = new Date(halloween);
 	const currentYear = new Date();
 
-	const untilHalloween = (halloweenDate - currentYear)
+	const untilHalloween = (halloweenDate - currentYear);
 
-	const days = Math.floor(untilHalloween / (24*60*60*1000))
-  	const hours = Math.floor(untilHalloween / (60*60*1000) % 24)
-  	const minutes = Math.floor(untilHalloween / (60*1000) % 60)
-  	const seconds = Math.floor(untilHalloween / 1000) % 60
+	const days = Math.floor(untilHalloween / (24*60*60*1000));
+  	const hours = Math.floor(untilHalloween / (60*60*1000)) % 24;
+  	const minutes = Math.floor(untilHalloween / (60*1000)) % 60;
+  	const seconds = Math.floor(untilHalloween / 1000) % 60;
 
 	daysEle.innerHTML = days;
 	hoursEle.innerHTML = hours;
@@ -26,4 +26,4 @@ function countdown() {
 
 countdown();
 
-setInterval(countdown, 1000)
+setInterval(countdown, 1000);
