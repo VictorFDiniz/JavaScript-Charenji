ddbcc11039d668771ff711c9302ac954c3f6c247
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2022-10-01 13:59:26 -0300

    initial commit

--- Countdown/Source/JavaScript/script.js
+++ Countdown/Source/JavaScript/script.js
@@ -1,29 +1,73 @@
-const daysEle = document.getElementById('days');
-const hoursEle = document.getElementById('hours');
-const minutesEle = document.getElementById('minutes');
-const secondsEle = document.getElementById('seconds');
+// Calcular tempo
 
-const halloween = "31 Oct 2022";
+function getTime(deadline) {
 
-function countdown() {
-	
-	const halloweenDate = new Date(halloween);
-	const currentYear = new Date();
-
-	const untilHalloween = (halloweenDate - currentYear);
+	const untilHalloween = (deadline - new Date());
 
 	const days = Math.floor(untilHalloween / (24*60*60*1000));
   	const hours = Math.floor(untilHalloween / (60*60*1000)) % 24;
   	const minutes = Math.floor(untilHalloween / (60*1000)) % 60;
   	const seconds = Math.floor(untilHalloween / 1000) % 60;
 
-	daysEle.innerHTML = days;
-	hoursEle.innerHTML = hours;
-	minutesEle.innerHTML = minutes;
-	secondsEle.innerHTML = seconds;
+  	return {
 
+  		'total': untilHalloween,
+  		'days': days,
+  		'hours': hours,
+  		'minutes': minutes,
+  		'seconds': seconds
+  	}
+	
 }
 
-countdown();
+// Iniciar cronômetro
 
-setInterval(countdown, 1000);
+function initCountdown(deadline) {
+
+	const daysEl = document.getElementById('days');
+	const hoursEl = document.getElementById('hours');
+	const minutesEl = document.getElementById('minutes');
+	const secondsEl = document.getElementById('seconds');
+
+
+	// Atualizar cronômetro
+
+	function updateCountdown() {
+
+		let untilH = getTime(deadline);
+
+		daysEl.innerHTML = untilH.days;
+		hoursEl.innerHTML = untilH.hours;
+		minutesEl.innerHTML = untilH.minutes;
+		secondsEl.innerHTML = untilH.seconds;
+
+		if (untilH.total <= 0) {
+			clearInterval(timeInterval)
+		}
+
+		// Verifica se o ano é bissexto
+
+		const year = new Date().getFullYear()
+		const isLeapYear = !((year % 4) && (year % 100) || !(year % 400))
+
+		if (year != isLeapYear && untilH.days == 0) {
+			halloweenDate = new Date(deadline.getTime() + 365 * 24 * 60 * 60 * 1000)
+			initCountdown(halloweenDate)
+			}
+
+		else if (year == isLeapYear && untilH.days == 0) {
+			halloweenDate = new Date(deadline.getTime() + 366 * 24 * 60 * 60 * 1000)
+			initCountdown(halloweenDate)
+
+		}
+
+	}
+
+	updateCountdown()
+	let timeInterval = setInterval(updateCountdown, 1000);
+}
+
+// Passando data inicial
+
+const halloweenDate = new Date('31 Oct 2022');
+initCountdown(halloweenDate)
