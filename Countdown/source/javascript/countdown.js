c3d05b40474d090d23edd4f05f874b4c778c3bbe
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2022-11-10 19:25:05 -0300

    refactor: countdown code

--- Countdown/source/javascript/countdown.js
+++ Countdown/source/javascript/countdown.js
@@ -1,6 +1,32 @@
-// Calcula tempo
+// configurando data inicial do contador
+const year = new Date().getFullYear()
 
-function getTime(deadline) {
+let timeInterval
+let deadline = new Date("Oct 31, " + year)
+
+// Função que inicia o contador
+function initCountdown() {
+
+	// Recebe o total de milissegundos como retorno de updateTime
+	const untilH = updateTime();
+
+	// Verfica se o ano é bissexto e se a data inicial expirou
+	const isLeapYear = !((year % 4) && (year % 100) || !(year % 400))
+	if (year != isLeapYear && untilH.total <= 0) {
+		deadline = new Date(deadline.getTime() + 365 * 24 * 60 * 60 * 1000)
+		updateTime()
+		}
+
+	else if (year == isLeapYear && untilH.total <= 0) {
+		deadline = new Date(deadline.getTime() + 366 * 24 * 60 * 60 * 1000)
+		updateTime()
+	}
+	// Executa a função updateTime a cada 1 segundo
+	timeInterval = setInterval(updateTime, 1000)
+}
+
+// Função que calcula os dias, horas, minutos e segundos até o halloween
+function updateTime() {
 
 	const untilHalloween = (deadline - new Date());
 
@@ -9,64 +35,15 @@ function getTime(deadline) {
   	const minutes = Math.floor(untilHalloween / (60*1000)) % 60;
   	const seconds = Math.floor(untilHalloween / 1000) % 60;
 
-  	return {
+	  document.getElementById('days').innerHTML = days
+	  document.getElementById('hours').innerHTML = hours
+	  document.getElementById('minutes').innerHTML = minutes
+	  document.getElementById('seconds').innerHTML = seconds
 
-  		'total': untilHalloween,
-  		'days': days,
-  		'hours': hours,
-  		'minutes': minutes,
-  		'seconds': seconds
-  	}
-	
+	  return {
+		'total': untilHalloween
+	  }
 }
 
-// Inicia cronômetro
-
-function initCountdown(deadline) {
-
-	const daysEl = document.getElementById('days');
-	const hoursEl = document.getElementById('hours');
-	const minutesEl = document.getElementById('minutes');
-	const secondsEl = document.getElementById('seconds');
-
-	// Atualiza cronômetro
-
-	function updateCountdown() {
-
-		let untilH = getTime(deadline);
-
-		daysEl.innerHTML = untilH.days;
-		hoursEl.innerHTML = untilH.hours;
-		minutesEl.innerHTML = untilH.minutes;
-		secondsEl.innerHTML = untilH.seconds;
-
-		if (untilH.total <= 0) {
-			clearInterval(timeInterval)
-		}
-
-		// Verifica se o ano é bissexto
-
-		const year = new Date().getFullYear()
-		const isLeapYear = !((year % 4) && (year % 100) || !(year % 400))
-
-		if (year != isLeapYear && untilH.total == 0) {
-			const deadline = new Date(deadline.getTime() + 365 * 24 * 60 * 60 * 1000)
-			initCountdown(deadline)
-			}
-
-		else if (year == isLeapYear && untilH.total == 0) {
-			const deadline = new Date(deadline.getTime() + 366 * 24 * 60 * 60 * 1000)
-			initCountdown(deadline)
-
-		}
-
-	}
-
-	updateCountdown()
-	let timeInterval = setInterval(updateCountdown, 1000);
-}
-
-// Passa data inicial
-
-const halloweenDate = new Date('31 Oct 2022');
-initCountdown(halloweenDate)
+// Chamada inicial
+initCountdown()
